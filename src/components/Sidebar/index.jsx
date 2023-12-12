ea6d258077676ca5430382eaf21a52b3dc5cff25
IslomJonMirzakbarov <islomjonmirzakbarov@gmail.com>
2023-12-12 17:31:43 +0500

    sidebar mobile view fixed

--- src/components/Sidebar/index.jsx
+++ src/components/Sidebar/index.jsx
@@ -1,9 +1,9 @@
 import styles from './style.module.scss'
 import { ReactComponent as Logo } from 'assets/logos/logoV2.svg'
-import { ReactComponent as BillingIcon } from 'assets/icons/billing.svg'
-import { ReactComponent as ProfileIcon } from 'assets/icons/profileq.svg'
 import { ReactComponent as GridIcon } from 'assets/icons/grid.svg'
 import { ReactComponent as PricingIcon } from 'assets/icons/pricing.svg'
+import { ReactComponent as BillingIcon } from 'assets/icons/billing.svg'
+import { ReactComponent as ProfileIcon } from 'assets/icons/profileq.svg'
 import { ReactComponent as LogoutIcon } from 'assets/icons/logout.svg'
 import { NavLink } from 'react-router-dom'
 import { useState } from 'react'
@@ -11,8 +11,11 @@ import LogoutModal from 'components/LogoutModal'
 import { useDashboard } from 'services/pool.service'
 
 import { useNavigate } from 'react-router-dom'
+import { Box } from '@mui/material'
+import Hamburger from 'hamburger-react'
+import MobileSidebar from './MobileSidebar'
 
-const items = [
+export const items = [
   {
     title: 'Dashboard',
     icon: <GridIcon />,
@@ -37,10 +40,12 @@ const items = [
 
 export default function Sidebar() {
   const [open, setOpen] = useState(false)
+  const [isOpen, setIsOpen] = useState(false)
   const { isLoading } = useDashboard()
   const navigate = useNavigate()
 
   const toggle = () => setOpen((prev) => !prev)
+  const toggleHamburger = () => setIsOpen((prev) => !prev)
 
   const handleNavigation = (path, event) => {
     if (isLoading) {
@@ -50,42 +55,59 @@ export default function Sidebar() {
     }
   }
 
+  const onClose = () => {
+    setIsOpen(false)
+    setOpen(false)
+  }
+
   return (
-    <div className={styles.sidebar}>
-      {open && <LogoutModal toggle={toggle} />}
-      <div>
-        <div className={styles.header}>
-          <NavLink to='/' onClick={(e) => handleNavigation('/', e)}>
-            <Logo style={{ width: 128, fontSize: 128 }} />
-          </NavLink>
+    <>
+      <div className={styles.sidebar}>
+        {open && <LogoutModal toggle={toggle} />}
+        <div>
+          <div className={styles.header}>
+            <NavLink to='/' onClick={(e) => handleNavigation('/', e)}>
+              <Logo style={{ width: 128, fontSize: 128 }} />
+            </NavLink>
+            <Box className={styles.burgerBtn}>
+              <Hamburger
+                size={20}
+                rounded
+                color='#fff'
+                toggled={isOpen}
+                toggle={toggleHamburger}
+              />
+            </Box>
+          </div>
+          <div className={styles.body}>
+            <nav>
+              <ul>
+                {items.map((item, key) => (
+                  <li key={key}>
+                    <NavLink
+                      className={(navData) =>
+                        navData.isActive ? styles.active : ''
+                      }
+                      to={item.path}
+                      onClick={(e) => handleNavigation(item.path, e)}
+                    >
+                      {item.icon}
+                      {item.title}
+                    </NavLink>
+                  </li>
+                ))}
+              </ul>
+            </nav>
+          </div>
         </div>
-        <div className={styles.body}>
-          <nav>
-            <ul>
-              {items.map((item, key) => (
-                <li key={key}>
-                  <NavLink
-                    className={(navData) =>
-                      navData.isActive ? styles.active : ''
-                    }
-                    to={item.path}
-                    onClick={(e) => handleNavigation(item.path, e)}
-                  >
-                    {item.icon}
-                    {item.title}
-                  </NavLink>
-                </li>
-              ))}
-            </ul>
-          </nav>
+        <div className={styles.footer}>
+          <div className={styles.logout} onClick={toggle}>
+            Log out
+            <LogoutIcon />
+          </div>
         </div>
       </div>
-      <div className={styles.footer}>
-        <div className={styles.logout} onClick={toggle}>
-          Log out
-          <LogoutIcon />
-        </div>
-      </div>
-    </div>
+      <MobileSidebar isOpen={isOpen} onClose={onClose} />
+    </>
   )
 }
