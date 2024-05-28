0af3b362e9b876b674e7ac3db8fe45cd9c9931b8
IslomJonMirzakbarov <islomjonmirzakbarov@gmail.com>
2024-05-28 14:34:38 +0500

    Pool Create Section created

---
+++ src/views/PoolCreate/index.jsx
@@ -0,0 +1,53 @@
+import { Box, Typography } from '@mui/material'
+import { useEffect } from 'react'
+import { useTranslation } from 'react-i18next'
+import { useNavigate } from 'react-router-dom'
+import poolStore from 'store/pool.store'
+import walletStore from 'store/wallet.store'
+import Pool from 'views/Billing/Pool'
+import { ReactComponent as MaxPoolsIcon } from 'assets/icons/max_pools_icon.svg'
+
+export default function PoolCreate() {
+  const navigate = useNavigate()
+  const { t } = useTranslation()
+  const walletAddress = walletStore.address
+  const { poolCount } = poolStore
+
+  useEffect(() => {
+    if (!walletAddress) {
+      navigate('/main/pool-creation/pool/connect-wallet')
+    } else {
+      navigate('/main/pool-creation/pool')
+    }
+  }, [navigate, walletAddress])
+
+  if (poolCount > 9) {
+    return (
+      <Box
+        display='flex'
+        alignItems='center'
+        marginTop='110px'
+        flexDirection='column'
+        gap='20px'
+      >
+        <MaxPoolsIcon />
+        <Typography
+          fontSize='18px'
+          color='red'
+          margin='18px'
+          fontWeight='bold'
+          textAlign='center'
+          sx={{
+            '@media (max-width: 600px)': {
+              fontSize: '14px'
+            }
+          }}
+        >
+          {t('pool_creation_limit_reached')}
+        </Typography>
+      </Box>
+    )
+  } else {
+    return <Pool />
+  }
+}
