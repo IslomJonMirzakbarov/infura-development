484f232ff8e302a470c70e4c6f23d8339eda3efa
IslomJonMirzakbarov <islomjonmirzakbarov@gmail.com>
2024-06-26 12:19:31 +0500

    made refactoring

--- src/views/Workspace/index.jsx
+++ src/views/Workspace/index.jsx
@@ -10,6 +10,7 @@ import { useGetPoolById } from 'services/pool.service'
 import FileButton from './FileButton'
 import GridListPicker from './GridListPicker'
 import styles from './style.module.scss'
+import { demoColumns, formattedData } from './customData'
 
 const fileButtons = [
   {
@@ -106,30 +107,7 @@ const Workspace = () => {
     menuAnchorEl
   }
 
-  const demoColumns = [
-    { key: 'name', title: 'Name' },
-    { key: 'type', title: 'Type (jpg, png, zip etc.)' },
-    { key: 'size', title: 'Size ' },
-    { key: 'created_at', title: 'Created Date' },
-    { key: 'content_id', title: 'Content ID' }
-  ]
-
-  const formattedData = [
-    {
-      name: 'pic11.zip',
-      type: 'application/zip',
-      size: '0.29MB',
-      created_at: 'June 26, 2024, 11:34:47',
-      content_id: 'bafybeicclcdj3opiobfvdm45h7aeku4owizymcgltffhg7fgv5mrk6sjfu'
-    },
-    {
-      name: 'pic12.zip',
-      type: 'application/zip',
-      size: '0.29MB',
-      created_at: 'June 26, 2024, 11:34:08',
-      content_id: 'bafybeia5nl2mtvutotghoctjeznhy2ct46dqmpvxtydoj5uyfrcfvz2y44'
-    }
-  ]
+  
 
   return (
     <Box>
