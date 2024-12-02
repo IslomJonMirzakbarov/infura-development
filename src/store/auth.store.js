6b332a518f2d608b4055ed7a8de7a37abc4e77f6
IslomJonMirzakbarov <islomjonmirzakbarov@gmail.com>
2024-12-02 09:51:32 +0500

    token setup safety updated

--- src/store/auth.store.js
+++ src/store/auth.store.js
@@ -31,14 +31,20 @@ class Store {
 
   setAccessToken(value) {
     if (!this.token.access) {
-      this.token.access = null
+      this.token.access = {
+        token: null,
+        expires: null
+      }
     }
     this.token.access.token = value
   }
 
   setRefreshToken(value) {
     if (!this.token.refresh) {
-      this.token.refresh = null
+      this.token.refresh = {
+        token: null,
+        expires: null
+      }
     }
     this.token.refresh.token = value
   }
